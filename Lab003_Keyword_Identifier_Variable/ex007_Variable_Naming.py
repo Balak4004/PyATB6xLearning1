03635ec20f2ed868591daa3f9df3a361b85fa88f
Bala <khotbalkrishna17@gmail.com>
2025-10-12 11:32:18 +0530

    commit

---
+++ Lab003_Keyword_Identifier_Variable/ex007_Variable_Naming.py
@@ -0,0 +1,16 @@
+
+
+_age = 45
+pi = 3.14
+abc123 = "Bob"
+isMale = True
+print(_age)
+print(pi)
+print(abc123)
+print(isMale)
+
+print(type(_age))
+print(type(pi))
+print(type(abc123))
+print(type(isMale))
+
