208f8be351d5bb8e85360bd0cf3d0328edc2a3a6
Bala <khotbalkrishna17@gmail.com>
2025-12-21 20:21:06 +0530

    commit on 20 Dev

--- Lab029_PyTest/test_ex173_GET_Request.py
+++ Lab029_PyTest/test_ex173_GET_Request.py
@@ -10,12 +10,13 @@ import requests
 def test_get_request():
     url = "https://restful-booker.herokuapp.com/booking/1"
     response_data = requests.get(url=url)
+    print(response_data)
     assert response_data.status_code==200
 
 @allure.title("TC#1 Verify the GET Resquest with invalid bookingid ")
 @allure.description("TC#1  testcase checking booking id -1 and status code is 200")
 @pytest.mark.positive
-def test_get_request():
+def test_get_request_negative():
     url = "https://restful-booker.herokuapp.com/booking/-1"
     response_data = requests.get(url=url)
     assert response_data.status_code==404
